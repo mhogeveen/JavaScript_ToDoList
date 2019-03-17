8cd738347148e730d844ccb8a2fe4525f81029f2
M. Hogeveen <mhogeveen94@gmail.com>
2019-03-17 15:37:18 +0100

    Content added to new list item when typed in input field

--- index.js
+++ index.js
@@ -2,11 +2,26 @@ function activateToDo() {
 
   let listItem = document.querySelector("#list__item");
   let list = document.querySelector("#list");
-  let listItemHtml = listItem.innerHTML;
   let addItem = document.querySelector("#button--addition");
+
 
   addItem.addEventListener("click", function() {
     let div = document.createElement("div");
+    let content = document.querySelector("#addition__input").value;
+
+    let listItemHtml = `<div class="item__content">\
+      <p>${content}</p>\
+    </div>\
+    <div class="item__buttons">\
+      <button class="button button--edit" type="button" name="button">\
+        <i class="far fa-edit"></i>\
+      </button>\
+      <button class="button button--remove" type="button" name="button">\
+        <i class="far fa-times-circle"></i>\
+      </button>\
+    </div>`;
+
+    console.log(content);
     div.className = "list__item";
     div.innerHTML = listItemHtml;
     list.appendChild(div);
