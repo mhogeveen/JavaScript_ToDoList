5f03a5e91b011ff522e67a4fb3fee6d9bafdec5d
M. Hogeveen <mhogeveen94@gmail.com>
2019-03-17 14:57:27 +0100

    Add list item button works

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 function activateToDo() {
-  
+
+  let listItem = document.querySelector("#list__item");
+  let list = document.querySelector("#list");
+  let listItemHtml = listItem.innerHTML;
+  let addItem = document.querySelector("#button--addition");
+
+  addItem.addEventListener("click", function() {
+    let div = document.createElement("div");
+    div.className = "list__item";
+    div.innerHTML = listItemHtml;
+    list.appendChild(div);
+  });
+
+
 }
