322728b8c323a7bc11819ddb3f97195f83677d7b
M. Hogeveen <mhogeveen94@gmail.com>
2019-03-18 20:44:06 +0100

    Add list item front-end and button click event

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 
+// User clicked on the add button
+// If there is any text inside the item field, add that text to the todo list
+document.querySelector('#add').addEventListener('click', function() {
+  let value = document.querySelector('#item').value;
+  if (value) {
+    console.log('There was a value!');
+  }
+});
 
 
 
