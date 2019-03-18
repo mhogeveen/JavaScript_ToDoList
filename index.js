1a86b1f884e949c560f993bf8f111c04276d5e5d
M. Hogeveen <mhogeveen94@gmail.com>
2019-03-18 19:55:22 +0100

    Add header and its styling

--- index.js
+++ index.js
@@ -1,38 +1,62 @@
-function activateToDo() {
-
-  let list = document.querySelector("#list");
-  let addItem = document.querySelector("#button--add");
-  let removeItem = document.querySelector(".button--remove");
-  let content = document.querySelector("#add__input");
-  let listItemHtml = document.querySelector("#list__item").innerHTML;
-  let existingItems = [1];
-  let count = 1;
-
-  function createItem() {
-    let div = document.createElement("div");
-    let newHTML = listItemHtml.split("placecontenthere").join(content.value);
-
-    if (content.value) {
-      content.value = '';
-      count++;
-      existingItems.push(count);
-      content.style.border = "1px solid black";
-      div.className = "list__item";
-      div.innerHTML = newHTML;
-      div.id = `item${existingItems[count - 1]}`;
-      div.querySelector(".item__buttons > .button--remove").id = `remove${existingItems[count -1]}`;
-      list.appendChild(div);
-    } else {
-      content.style.border = "1px solid red";
-    }
-  }
-
-  removeItem.addEventListener("click", function() {
 
 
-  });
 
-  addItem.addEventListener("click", function() {
-    createItem();
-  });
-}
+
+
+
+
+
+// !!!!!! This was my own attempte at writing the code!!!!!!
+// function activateToDo() {
+//
+//   let button = document.querySelector('button');
+//   let input = document.querySelector('input');
+//   let ul = document.querySelector('ul');
+//   let item = document.querySelectorAll('li');
+//
+//   function inputLength() {
+//     return input.value.length;
+//   }
+//
+//   function listLength() {
+//     return item.length;
+//   }
+//
+//   function createListElement() {
+//     let li = document.createElement('li');
+//     li.appendChild(document.createTextNode(input.value));
+//     ul.appendChild(li);
+//     input.value = '';
+//
+//     function complete() {
+//       li.classList.toggle('complete');
+//     }
+//
+//     li.addEventListener('click', complete());
+//
+//     let deleteButton = document.createElement('button');
+//     deleteButton.appendChild(document.createTextNode('X'));
+//     li.appendChild(deleteButton);
+//     deleteButton.addEventListener('click', deleteItem);
+//
+//     function deleteItem() {
+//       li.classList.add('delete');
+//     }
+//   }
+//
+//   function addListAfterClick() {
+//     if (inputLength() > 0) {
+//       createListElement();
+//     }
+//   }
+//
+//   function addListAfterKeypress(event) {
+//     if (inputLength() > 0 && event.keypress === 13) {
+//       createListElement();
+//     }
+//   }
+//
+//   button.addEventListener('click', addListAfterClick());
+//
+//   input.addEventListener('keypress', addListAfterKeypress());
+// }
