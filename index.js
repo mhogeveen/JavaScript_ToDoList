1337a4495ab008e48fd2276cc54c732fe10cfa8b
M. Hogeveen <mhogeveen94@gmail.com>
2019-03-18 22:39:16 +0100

    Add delete list item functionality

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
+// Remove and complete icons HTML
+let removeIcon = '<i class="far fa-trash-alt"></i>';
+let completeIcon = '<i class="fas fa-check"></i>';
 
 // User clicked on the add button
 // If there is any text inside the item field, add that text to the todo list
 document.querySelector('#add').addEventListener('click', function() {
   let value = document.querySelector('#item').value;
   if (value) {
-    console.log('There was a value!');
+    addItemTodo(value);
+    document.querySelector('#item').value = '';
   }
 });
 
+function removeItem() {
+  let item = this.parentNode.parentNode;
+  let parent = item.parentNode;
 
+  parent.removeChild(item);
+}
+
+// Adds a new item to the todo list
+function addItemTodo(text) {
+  let list = document.querySelector('#todo');
+
+  let item = document.createElement('li');
+  item.innerText = text;
+
+  let buttons = document.createElement('div');
+  buttons.classList.add('todo__buttons');
+
+  let remove = document.createElement('button');
+  remove.classList.add('buttons__remove');
+  remove.innerHTML = removeIcon;
+
+  // Add event listener for removing an item
+  remove.addEventListener('click', removeItem);
+
+  let complete = document.createElement('button');
+  complete.classList.add('buttons__complete');
+  complete.innerHTML = completeIcon;
+
+  buttons.appendChild(remove);
+  buttons.appendChild(complete);
+  item.appendChild(buttons);
+
+  list.insertBefore(item, list.childNodes[0]);
+}
 
 
 
