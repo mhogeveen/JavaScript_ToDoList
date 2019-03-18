a103280b130f95417cf8ace21bcd479248a48cae
M. Hogeveen <mhogeveen94@gmail.com>
2019-03-18 11:59:32 +0100

    Add item# class on creation of new list__item

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ function activateToDo() {
 
   let list = document.querySelector("#list");
   let addItem = document.querySelector("#button--add");
-  let removeItem = document.querySelector("#button--remove");
+  let removeItem0 = document.querySelector("button[name='remove0']");
   let content = document.querySelector("#add__input");
   let listItemHtml = document.querySelector("#list__item").innerHTML;
+  let existingItems = 0;
 
   addItem.addEventListener("click", function() {
     let div = document.createElement("div");
@@ -12,8 +13,10 @@ function activateToDo() {
     let newHTML = listItemHtml.split("placecontenthere").join(contentVal);
 
     if (contentVal) {
+      existingItems++;
+      console.log(existingItems);
       content.style.border = "1px solid black";
-      div.className = "list__item";
+      div.className = `list__item item${existingItems}`;
       div.innerHTML = newHTML;
       list.appendChild(div);
     } else {
@@ -21,8 +24,9 @@ function activateToDo() {
     }
   });
 
-  removeItem.addEventListener("click", function() {
-    list.removeChild("#list__item");
+  removeItem0.addEventListener("click", function() {
+
     console.log("hello");
+    list.removeChild("div.template");
   });
 }
