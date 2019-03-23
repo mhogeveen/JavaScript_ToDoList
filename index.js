def069633651979241c8c0051356536de3952aa2
M. Hogeveen <mhogeveen94@gmail.com>
2019-03-23 19:14:02 +0100

    Alert shown when trying to access list items without a list available

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
-// Set data object for storage of todo and completed values
-let data = (localStorage.getItem('todoList')) ? JSON.parse(localStorage.getItem('todoList')) :  {
-  todo: [],
-  completed: [],
-};
+// Set data object for storage of list items with there respective todo and completed values
+let data = (localStorage.getItem('todoList')) ? JSON.parse(localStorage.getItem('todoList')) : {};
 
-let lists = (localStorage.getItem('lists')) ? JSON.parse(localStorage.getItem('lists')) : {
-  lists: [],
-};
-
-// Remove and complete icons HTML
+// Remove and complete icons html
 let removeIcon = '<i class="far fa-trash-alt"></i>';
 let completeIcon = '<i class="fas fa-check"></i>';
 
-renderTodoList();
-renderListsList();
-
+// If there is any text inside the item field, add that text to the todo list of it's own list object
 // User clicked on the add button
-// If there is any text inside the item field, add that text to the todo list
 document.querySelector('#add').addEventListener('click', function() {
   let value = document.querySelector('#item').value;
   if (value) {
@@ -24,131 +14,59 @@ document.querySelector('#add').addEventListener('click', function() {
   }
 });
 
+// User pressed the enter key
 document.querySelector('#item').addEventListener('keydown', function(event) {
-  let value = this.value;
-  if (event.code === 'Enter' && value) {
-    addItem(value);
-  }
+
 });
 
-// User clicked on the cat-add button
-// If there is any text inside the categorie field, add that text to the list list
+// If there is any text inside the categorie field, add that text to the todo list of it's own list object
+// User clicked on the categorie button
 document.querySelector('#cat-add').addEventListener('click', function() {
-  let value = document.querySelector('#categorie').value;
-  if (value) {
-    addList(value);
-  }
+
 });
 
+// User pressed the enter key
 document.querySelector('#categorie').addEventListener('keydown', function(event) {
-  let value = this.value;
-  if (event.code === 'Enter' && value) {
-    addList(value);
-  }
+
 });
 
 function addItem(value) {
   addItemToDOM(value);
   document.querySelector('#item').value = '';
-
-  data.todo.push(value);
-  dataObjectUpdated();
 }
 
 function addList(value) {
-  addListToDOM(value);
-  document.querySelector('#categorie').value = '';
 
-  lists.lists.push(value);
-  listsObjectUpdated();
-  console.log(lists);
 }
 
 function renderTodoList() {
-  if (!data.todo.length && !data.completed.length) return;
 
-  for (let i = 0; i < data.todo.length; i++) {
-    let value = data.todo[i];
-    addItemToDOM(value);
-  }
-
-  for (let j = 0; j < data.completed.length; j++) {
-    let value = data.completed[j];
-    addItemToDOM(value, true);
-  }
 }
 
 function renderListsList() {
-  if (!lists.lists.length) return;
 
-  for (let i = 0; i < lists.lists.length; i++) {
-    let value = lists.lists[i];
-    addListToDOM(value);
-  }
 }
 
 function dataObjectUpdated() {
-  localStorage.setItem('todoList', JSON.stringify(data));
+
 }
 
 function listsObjectUpdated() {
-  localStorage.setItem('lists', JSON.stringify(lists));
+
 }
 
 function removeItem() {
-  let item = this.parentNode.parentNode;
-  let parent = item.parentNode;
-  let id = parent.id;
-  let value = item.innerText;
 
-  if (id === 'todo') {
-    data.todo.splice(data.todo.indexOf(value), 1);
-  } else {
-    data.completed.splice(data.completed.indexOf(value), 1);
-  }
-
-  dataObjectUpdated();
-
-  parent.removeChild(item);
 }
 
 function removeList() {
-  let item = this.parentNode.parentNode;
-  let parent = item.parentNode;
-  let id = parent.id;
-  let value = item.innerText;
 
-  if (id === 'list') {
-    lists.lists.splice(lists.lists.indexOf(value), 1);
-  }
-
-  listsObjectUpdated();
-
-  parent.removeChild(item);
 }
 
 function completeItem() {
-  let item = this.parentNode.parentNode;
-  let parent = item.parentNode;
-  let id = parent.id;
-  let value = item.innerText;
 
-  if (id === 'todo') {
-    data.todo.splice(data.todo.indexOf(value), 1);
-    data.completed.push(value);
-  } else {
-    data.completed.splice(data.completed.indexOf(value), 1);
-    data.todo.push(value);
-  }
-
-  dataObjectUpdated();
-
-  // Check if the item should be added to the completed list or re-added to the todo list
-  let target = (id === 'todo') ? document.querySelector('#completed') : document.querySelector('#todo');
-
-  parent.removeChild(item);
-  target.insertBefore(item, target.childNodes[0]);
 }
+
 
 // Adds a new item to the todo list
 function addItemToDOM(text, completed) {
@@ -181,6 +99,7 @@ function addItemToDOM(text, completed) {
   list.insertBefore(item, list.childNodes[0]);
 }
 
+// Adds new item to the lists list
 function addListToDOM(text) {
   let list = document.querySelector('#list');
 
@@ -203,6 +122,17 @@ function addListToDOM(text) {
   list.insertBefore(item, list.childNodes[0]);
 }
 
+// Check if data object is empty
+function isEmpty(obj) {
+  for (let prop in obj) {
+    if (obj.hasOwnProperty(prop)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// Menu sliding functionality
 let menuButtons = document.querySelectorAll('.button');
 let panelCover = document.querySelector('.panel__cover');
 
@@ -212,13 +142,21 @@ menuButtons.forEach(function(button) {
       document.querySelector('.menu-panel').style.right = '0';
       document.querySelector('.list-panel').style.opacity = '0.5';
     } else if (this.classList.contains('button--listMenu')) {
-      document.querySelector('.menu-panel').style.right = '-100%';
-      document.querySelector('.list-panel').style.opacity = '1';
+      if (isEmpty(data)) {
+        document.querySelector('.alert').style.opacity = '1';
+      } else {
+        document.querySelector('.menu-panel').style.right = '-100%';
+        document.querySelector('.list-panel').style.opacity = '1';
+      }
     }
   });
 });
 
 panelCover.addEventListener('click', function() {
-  document.querySelector('.menu-panel').style.right = '-100%';
-  document.querySelector('.list-panel').style.opacity = '1';
+  if (isEmpty(data)) {
+    document.querySelector('.alert').style.opacity = '1';
+  } else {
+    document.querySelector('.menu-panel').style.right = '-100%';
+    document.querySelector('.list-panel').style.opacity = '1';
+  }
 });
